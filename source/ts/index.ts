d25a027138bec12ce1fa06b37761a235adde14f6
Natalie Weizenbaum <nex342@gmail.com>
2022-11-20 22:34:53 +0000

    Add basic highlighting

--- source/ts/index.ts
+++ source/ts/index.ts
@@ -1,1 +1,29 @@
-console.log('hello, world');
+import Prism from 'prismjs';
+import {MDCTextField} from '@material/textfield';
+import {MDCSelect} from '@material/select';
+
+Prism.manual = true;
+
+function debounce<T extends Function>(fn: T, wait = 200): T {
+    let timeout = setTimeout(() => {}, 0);
+    let callable = (...args: any) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => fn(...args), wait);
+    };
+    return callable as unknown as T;
+}
+
+const input = document.querySelector("#input") as HTMLElement;
+const inputText = new MDCTextField(input.querySelector(".mdc-text-field")!);
+
+const output = document.querySelector("#output") as HTMLElement;
+const outputText = output.querySelector("code") as HTMLElement;
+inputText.listen('input', debounce(e => {
+    const source = (e.target as HTMLInputElement).value;
+    outputText.innerHTML = Prism.highlight(source, Prism.languages.css, 'css');
+}));
+
+const selectTheme = new MDCSelect(document.querySelector('#select-theme')!);
+selectTheme.listen('MDCSelect:change', () => {
+    output.dataset.theme = selectTheme.value;
+});
