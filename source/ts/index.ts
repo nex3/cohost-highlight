886104d8559f2c3bc998d34d35f56f3558efc4fc
Natalie Weizenbaum <nex342@gmail.com>
2022-11-21 01:38:23 +0000

    Add a Copy HTML button

--- source/ts/index.ts
+++ source/ts/index.ts
@@ -1,6 +1,8 @@
+import escapeHtml from 'escape-html';
 import hljs from 'highlightjs';
 import {MDCTextField} from '@material/textfield';
 import {MDCSelect} from '@material/select';
+import {MDCRipple} from '@material/ripple';
 
 function debounce<T extends Function>(fn: T, wait = 200): T {
   let timeout = setTimeout(() => {}, 0);
@@ -15,6 +17,7 @@ const input = document.querySelector('#input') as HTMLElement;
 const inputText = new MDCTextField(input.querySelector('.mdc-text-field')!);
 const selectLanguage = new MDCSelect(document.querySelector('#select-language')!);
 const output = document.querySelector('#output') as HTMLElement;
+const outputPre = output.querySelector('pre') as HTMLElement;
 const outputText = output.querySelector('code') as HTMLElement;
 
 function highlight() {
@@ -24,6 +27,7 @@ function highlight() {
       ? hljs.highlightAuto(source)
       : hljs.highlight(selectLanguage.value, source)
   ).value;
+  outputText.innerHTML += '<span id="baseline"></span>';
 }
 
 selectLanguage.listen('MDCSelect:change', highlight);
@@ -33,3 +37,40 @@ const selectTheme = new MDCSelect(document.querySelector('#select-theme')!);
 selectTheme.listen('MDCSelect:change', () => {
   output.dataset.theme = selectTheme.value;
 });
+
+const allCss = Array.from(document.styleSheets).map(stylesheet => Array.from(stylesheet.cssRules ?? []).filter(rule => rule instanceof CSSStyleRule)  as CSSStyleRule[]).flat();
+function getMatchedCSSRules(element: HTMLElement): Set<CSSStyleRule> {
+    return new Set(allCss.filter(rule => element.matches(rule.selectorText)));
+}
+
+function getStyleDiff(baseline: Set<CSSStyleRule>, element: HTMLElement): string {
+    const decls: string[] = [];
+    for (const rule of getMatchedCSSRules(element)) {
+        if (baseline.has(rule)) continue;
+        for (let i =0 ; i < rule.style.length; i ++) {
+            const name = rule.style[i];
+            const value = rule.style.getPropertyValue(name);
+            decls.push(`${name}: ${value}`);
+        }
+    }
+    return decls.join(';');
+}
+
+const copyHtml = new MDCRipple(document.querySelector('#copy-html')!);
+copyHtml.listen('click', async () => {
+    const pre = window.getComputedStyle(outputPre);
+    const baseline = getMatchedCSSRules(document.querySelector("#baseline")!);
+    const html = `<pre style="
+        background-color: ${pre.backgroundColor};
+        color: ${pre.color};
+    ">` + 
+    Array.from(outputText.childNodes).map(child => {
+        if (child instanceof Text) return child.nodeValue;
+        const span = child as HTMLSpanElement;
+        if (span.id === 'baseline') return '';
+        return '<span style="' + getStyleDiff(baseline, span) + '">' + escapeHtml(span.innerText) + '</span>';
+    }).join('') + '</pre>';
+    await navigator.clipboard.writeText(html);
+    // this._snackBar.open("HTML copied!", undefined, { duration: 1000 });
+})
+
