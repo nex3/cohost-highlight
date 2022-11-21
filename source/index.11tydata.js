41c97a5c3c7b3a28e9b22dfd6b8e2fee11455f92
Natalie Weizenbaum <nex342@gmail.com>
2022-11-20 20:12:38 -0800

    Change the defauilt style

--- source/index.11tydata.js
+++ source/index.11tydata.js
@@ -3,15 +3,16 @@ const hljs = require("highlightjs");
 const titleize = require("titleize");
 
 module.exports = {
-eleventyComputed: {
-    languages: hljs.listLanguages,
-    themes: () => {
-        const ids = Array.from(fs.readdirSync("node_modules/highlightjs/styles")
-            .filter(path => path.endsWith('.css'))
-            .map(path => path.substring(0, path.length - 4))
-            .filter(id => id !== 'default'));
-        ids.sort();
-        return [...ids.map(id => ({id, name: titleize(id.replace(/[-_.]/g, ' '))}))]
+    defaultTheme: {id: 'atelier-forest.dark', name: 'Atelier Forest Dark'},
+    eleventyComputed: {
+        languages: hljs.listLanguages,
+        themes: () => {
+            const ids = Array.from(fs.readdirSync("node_modules/highlightjs/styles")
+                .filter(path => path.endsWith('.css'))
+                .map(path => path.substring(0, path.length - 4))
+                .filter(id => id !== 'default'));
+            ids.sort();
+            return [...ids.map(id => ({id, name: titleize(id.replace(/[-_.]/g, ' '))}))]
+        }
     }
-}
 };
